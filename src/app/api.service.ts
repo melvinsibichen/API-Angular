4fe4c469e8062b633ec4728bdb2cfe7134e86cda
melvinsibichen <melvinsibichen1020@gmail.com>
2022-11-29 23:30:35 +0530

    fetch added  usdata

---
+++ src/app/api.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private http:HttpClient) { }
+  passengerData =()=>{
+    return this.http.get('https://api.instantwebtools.net/v1/passenger?page=0&size=30')
+  }
+  usPublicData = ()=>{
+    return this.http.get('https://datausa.io/api/data?drilldowns=Nation&measures=Population')
+  }
+}
