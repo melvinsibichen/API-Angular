3bb472111b06ede40e13fbed1209e8c95346f801
melvinsibichen <melvinsibichen1020@gmail.com>
2022-11-29 09:46:22 +0530

    passnger

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PassengerviewComponent } from './passengerview/passengerview.component';
+
+const myRoutes:Routes=[
+  {
+  path:"",
+  component:PassengerviewComponent
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PassengerviewComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
